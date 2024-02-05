7751a66e4b1c603666c158a9b5c66256645219e3
tot0p <thomas.lemaitre.78420@gmail.com>
2024-02-05 10:05:11 +0100

    add request method

--- vutlr/vutlr.go
+++ vutlr/vutlr.go
@@ -1,5 +1,10 @@
 package vutlr
 
+import (
+	"io"
+	"net/http"
+)
+
 type Vutlr struct {
 	APIKey string
 }
@@ -10,4 +15,55 @@ func New() *Vutlr {
 
 func (v *Vutlr) SetAPIKey(key string) {
 	v.APIKey = key
+}
+
+type request struct {
+	URL    string
+	method string
+	body   io.Reader
+}
+
+func newRequest(url string, method string, body io.Reader) request {
+	return request{
+		URL:    url,
+		method: method,
+		body:   body,
+	}
+}
+
+func newRequestNoBody(url string, method string) request {
+	return request{
+		URL:    url,
+		method: method,
+		body:   nil,
+	}
+}
+
+func (v *Vutlr) request(r request) *http.Response {
+	var bearer = "Bearer " + v.APIKey
+
+	req, err := http.NewRequest(r.method, r.URL, r.body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Add("Authorization", bearer)
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+
+	return resp
 }
