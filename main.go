c10a9360cc16951bea6bd6d9b16d5e32976ee5b8
tot0p <thomas.lemaitre.78420@gmail.com>
2024-02-05 12:15:02 +0100

    init bubble tea

--- main.go
+++ main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/tot0p/env"
 	"runner/vutlr"
 )
+
+type Model struct {
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c", "esc":
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+	}
+	return m, nil
+}
+
+func (m Model) View() string {
+	return "Hello, Bubble Tea!"
+}
 
 func init() {
 	err := env.Load()
 	if err != nil {
 		panic(err)
 	}
+
 }
 
 func main() {
 	api := vutlr.New()
 	api.SetAPIKey(env.Get("API_KEY"))
+
+	p := tea.NewProgram(Model{})
+	if _, err := p.Run(); err != nil {
+		panic(err)
+	}
 
 	/*
 		js, err := os.ReadFile("vm.json")
