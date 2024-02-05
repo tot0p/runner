314154161f99dfdaaed8af3b66d4c9c387addd19
tot0p <thomas.lemaitre.78420@gmail.com>
2024-02-05 12:47:21 +0100

    v1 of cli

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/tot0p/env"
+	"os"
+	"os/signal"
 	"runner/vutlr"
+	"strings"
+	"time"
 )
 
 func init() {
@@ -17,23 +22,74 @@ func main() {
 	api := vutlr.New()
 	api.SetAPIKey(env.Get("API_KEY"))
 
-	/*
-		js, err := os.ReadFile("vm.json")
-		if err != nil {
-			panic(err)
-		}
-		i, err := api.CreateInstance(string(js))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(i.ID)
+	js, err := os.ReadFile("vm.json")
+	if err != nil {
+		panic(err)
+	}
+	i, err := api.CreateInstance(string(js))
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Println("Creating instance... ", i.ID)
+
+	lastStatus := "none"
+	for i.ServerStatus != "ok" {
+		i, err = api.GetInstance(i.ID)
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(1 * time.Second)
+		if i.ServerStatus != lastStatus {
+			fmt.Println(i.ServerStatus)
+			lastStatus = i.ServerStatus
+		}
+	}
+
+	fmt.Println("Instance created")
+
+	// handle ctrl+c
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		err := api.DeleteInstance(i.ID)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Instance deleted")
+		os.Exit(0)
+	}()
+
+	for {
+		fmt.Print(">>")
+		var cmd string
+		_, err := fmt.Scanln(&cmd)
+		if err != nil {
+			err2 := api.DeleteInstance(i.ID)
+			if err2 != nil {
+				panic(err2)
+			}
+			fmt.Println("Instance deleted")
+			panic(err)
+		}
+		cmd = strings.ToLower(cmd[:len(cmd)-1])
+		switch cmd {
+		case "quit":
+			err := api.DeleteInstance(i.ID)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("Instance deleted")
+			os.Exit(0)
+		default:
+			fmt.Println("Unknown command")
+		}
+	}
+
+	/*
 		lst := api.ListInstances()
-		fmt.Println(lst.Meta.Total)
 
-	*/
-
-	/*
 		id := lst.Instances[0].ID
 
 		fmt.Println(id)
